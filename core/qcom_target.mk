99c2ee960ca090f2f9f031585397d2943b71df45
Anthony King <anthonydking@slimroms.net>
2014-11-17 22:06:31 +0000

    pathmap: Point QC HAL pathmaps directly to the source
    Directly map to the actual HAL directory, including the
    board platform. This lets project-path-for point directly
    to the respective HALs.
    
    Change-Id: Ic4ed61bbdea9d0b5683502bf84a8410e76858527

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -23,17 +23,17 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
     endif
 
-$(call project-set-path,qcom-audio,hardware/qcom/audio-caf)
+$(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,CAMERA,camera)
-$(call project-set-path,qcom-display,hardware/qcom/display-caf)
+$(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,GPS,gps)
-$(call project-set-path,qcom-media,hardware/qcom/media-caf)
+$(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,SENSORS,sensors)
 else
-$(call project-set-path,qcom-audio,hardware/qcom/audio)
+$(call project-set-path,qcom-audio,hardware/qcom/audio/default)
 $(call qcom-set-path-variant,CAMERA,camera)
-$(call project-set-path,qcom-display,hardware/qcom/display)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,GPS,gps)
-$(call project-set-path,qcom-media,hardware/qcom/media)
+$(call project-set-path,qcom-media,hardware/qcom/media/default)
 $(call qcom-set-path-variant,SENSORS,sensors)
 endif
