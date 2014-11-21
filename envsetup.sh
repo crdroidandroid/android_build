72aec6ed4252457eaae78edb0820345933448021
cristianomatos <cristianobmatos@gmail.com>
2014-11-20 23:19:44 -0200

    There's no bacon no more! Now is crdroid!!!

--- envsetup.sh
+++ envsetup.sh
@@ -495,7 +495,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka crdroid
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
