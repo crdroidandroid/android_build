03b94160b383ca669303abef770bfcfb1df7b5c9
mydongistiny <jaysonedson@gmail.com>
2016-01-05 13:16:43 -0800

    build: Remove annoying text when flashing
    Change-Id: If370c74000754abd51dfde221f4146bbf75f2327
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1514,7 +1514,8 @@ class BlockDifference(object):
   def WriteScript(self, script, output_zip, progress=None):
     if not self.src:
       # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
+      script.Print(" ")
+      script.Print("Flashing System..")
     else:
       script.Print("Patching %s image after verification." % (self.partition,))
 
@@ -1626,7 +1627,7 @@ class BlockDifference(object):
 
   def _WritePostInstallVerifyScript(self, script):
     partition = self.partition
-    script.Print('Verifying the updated %s image...' % (partition,))
+    #script.Print('Verifying the updated %s image...' % (partition,))
     # Unlike pre-install verification, clobbered_blocks should not be ignored.
     ranges = self.tgt.care_map
     ranges_str = ranges.to_string_raw()
@@ -1641,7 +1642,8 @@ class BlockDifference(object):
       script.AppendExtra('if range_sha1("%s", "%s") == "%s" then' % (
                          self.device, ranges_str,
                          self._HashZeroBlocks(self.tgt.extended.size())))
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Verified System..")
       if partition == "system":
         code = ErrorCode.SYSTEM_NONZERO_CONTENTS
       else:
@@ -1652,7 +1654,8 @@ class BlockDifference(object):
           'OTA update");\n'
           'endif;' % (code, partition))
     else:
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Verified System..")
 
     if partition == "system":
       code = ErrorCode.SYSTEM_UNEXPECTED_CONTENTS
