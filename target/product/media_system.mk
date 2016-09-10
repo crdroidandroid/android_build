c5ac4933dc7f88ccceacac0b9a73bec5193eda2e
ezio84 <brabus84@gmail.com>
2016-09-10 14:33:43 +0200

    Don't build useless html viewer
    Change-Id: If36d42d094c768d19039da80234499aeac56459e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- target/product/media_system.mk
+++ target/product/media_system.mk
@@ -29,7 +29,6 @@ PRODUCT_PACKAGES += \
     drmserver \
     ethernet-service \
     fsck.f2fs \
-    HTMLViewer \
     libfilterpack_imageproc \
     libwebviewchromium_loader \
     libwebviewchromium_plat_support \
