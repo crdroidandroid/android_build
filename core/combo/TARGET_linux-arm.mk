eb67b545590b26dd1c3471c349962f82d6bb3f7e
mydongistiny <jaysonedson@gmail.com>
2016-09-19 22:52:54 -0700

    build: Use -march=native for host
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -148,7 +148,7 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden
 # More flags/options can be added here
 $(combo_2nd_arch_prefix)TARGET_RELEASE_CFLAGS := \
 			-DNDEBUG \
-			-g \
+			-g0 \
 			-Wstrict-aliasing=2 \
 			-fgcse-after-reload \
 			-frerun-cse-after-loop \
