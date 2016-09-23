a31d4d54b8f7ecb545eeb8d61aed2a3b4f7a5c96
firebird11 <hbgassel@gmail.com>
2016-09-23 13:34:19 +0200

    build: crdroidify
    Change-Id: I35820cdc13a7c3d2f2696ad15a005d9bbeae8334
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -3,8 +3,6 @@
 print_build_config_vars := \
   PLATFORM_VERSION_CODENAME \
   PLATFORM_VERSION \
-  LINEAGE_VERSION \
-  TARGET_PRODUCT \
   TARGET_BUILD_VARIANT \
   TARGET_BUILD_TYPE \
   TARGET_BUILD_APPS \
@@ -131,6 +129,8 @@ endif # CALLED_FROM_SETUP
 
 ifneq ($(PRINT_BUILD_CONFIG),)
 $(info ============================================)
+$(info   CRDROID_VERSION=$(LINEAGE_VERSION))
+$(info   CRDROID_DEVICE=$(CM_BUILD))
 $(foreach v, $(print_build_config_vars),\
   $(info $v=$($(v))))
 $(info ============================================)
--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -66,5 +66,6 @@ fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
 echo "ro.cm.device=$CM_DEVICE"
+echo "ro.crdroid.device=$CM_DEVICE"
 
 echo "# end build properties"
