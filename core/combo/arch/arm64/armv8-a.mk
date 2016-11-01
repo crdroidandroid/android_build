d1b9f2fd52a1666ba0b693d79709fd351e84483d
Alex Naidis <alex.naidis@linux.com>
2016-11-01 14:39:16 +0100

    arm64: Rework A53 Errata Fixing logic
    This commit introduces an internal variable
    "APPLY_A53_ERRATA_FIXES" which will be the final
    indicator on whether to apply the Errata fixes
    for Cortex-A53 or not. Since the variable is internal
    only, it gets cleared before and after usage.
    
    Additionally to the CPU detection logic, we are
    now reading "TARGET_IS_CORTEX-A53" which is a flag
    that CAF sets to true on chipsets which contain
    a Cortex-A53, even if they are using "generic"
    as their CPU target. So this is the only way
    to find out whether an Cortex-A53 is really
    present.
    
    We are overriding any results from the CPU
    target detection with what comes from
    "TARGET_IS_CORTEX-A53" if it is present now.
    
    That means that, we are also introducing a
    way to force opting in and force opting out of
    the Cortex-A53 Errata fixes.
    
    Additionally the CPU detection logic
    is deduplicated and cleaned up.
    
    Change-Id: I1f46cdc41087c4b45c365211be5e9ef0bbdc97f1
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,4 +1,17 @@
+APPLY_A53_ERRATA_FIXES :=
 
-ifeq (,$(filter cortex-a53 default,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+ifeq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	APPLY_A53_ERRATA_FIXES := true
+endif
+endif
+
+ifneq ($(strip $(TARGET_IS_CORTEX-A53)),)
+	APPLY_A53_ERRATA_FIXES := $(TARGET_IS_CORTEX-A53)
+endif
+
+ifneq ($(APPLY_A53_ERRATA_FIXES),true)
 	RS_DISABLE_A53_WORKAROUND := true
 endif
+
+APPLY_A53_ERRATA_FIXES :=
