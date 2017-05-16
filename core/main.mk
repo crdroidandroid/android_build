56d6b4fa475a55f814972106c8068133cb9957e0
ezio84 <brabus84@gmail.com>
2017-05-16 12:00:35 +0200

    Allow adbd root on userdebug builds
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -360,15 +360,16 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),user)
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
   endif
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
     tags_to_install += debug
   else
     # Disable debugging in plain user builds.
