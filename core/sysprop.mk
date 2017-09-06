73e90f05e1323faf497b340768fa5396a62f2b1e
neobuddy89 <neobuddy89@gmail.com>
2017-09-07 02:17:44 +0530

    Make build ID simple
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -218,7 +218,7 @@ BUILD_THUMBPRINT :=
 #
 
 # BUILD_ID: detail info; has the same info as the build fingerprint
-BUILD_DESC := $(TARGET_PRODUCT)-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER_FROM_FILE) $(BUILD_VERSION_TAGS)
+BUILD_DESC := $(BUILD_ID)
 
 # BUILD_DISPLAY_ID is shown under Settings -> About Phone
 ifeq ($(TARGET_BUILD_VARIANT),user)
