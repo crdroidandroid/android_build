fa3ff972067f3f78f41ca29737c4673ebfd4bf30
Pranav Vashi <neobuddy89@gmail.com>
2017-09-10 15:07:23 +0530

    Revert "HACK: releasetools: don't assert device"
    This reverts commit c9478c96142c1916d4f2df2dcc4e6593d29b3be2.

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -145,7 +145,13 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    pass
+    cmd = ('assert(' +
+           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+                         % (i, i) for i in device.split(",")]) +
+           ' || abort("E%d: This package is for device: %s; ' +
+           'this device is " + getprop("ro.product.device") + ".");' +
+           ');') % (common.ErrorCode.DEVICE_MISMATCH, device)
+    self.script.append(self.WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
     """Assert that the bootloader version is one of *bootloaders."""
