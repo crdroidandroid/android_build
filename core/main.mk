eb29b4e846eb9c750d651dfc0110209c61303e24
Pranav Vashi <neobuddy89@gmail.com>
2018-08-27 18:08:29 +0530

    DNM: Allow adb debug
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -277,7 +277,7 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
 
   ifeq ($(user_variant),user)
