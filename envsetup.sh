67224e4a20e0dc70200b6a15cc5d522c67b7304d
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-09-06 18:31:15 +0200

    Automatically set CCACHE_EXEC to the system's ccache
    * Check for ccache tool and set its path in CCACHE_EXEC if it hasn't been set already
    
    * Enable ccache and compression by default
    
    Change-Id: Ife9d0a723cb90b89392ee288ebe6e7e9f2be7eef

--- envsetup.sh
+++ envsetup.sh
@@ -1889,10 +1889,12 @@ addcompletions
 if [ -z ${CCACHE_EXEC} ]; then
     ccache_path=$(which ccache)
     if [ ! -z "$ccache_path" ]; then
+        export USE_CCACHE=1
+        export CCACHE_COMPRESS=1
         export CCACHE_EXEC="$ccache_path"
-        echo "ccache found and CCACHE_EXEC has been set to : $ccache_path"
+        echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
     else
-        echo "ccache not found/installed!"
+        echo -e "\e[31m\e[1mccache not found/installed!\e[0m"
     fi
 fi
 
