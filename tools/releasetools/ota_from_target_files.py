102c622f93e252a504e09f777282740070fb7e03
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:04:03 +0530

    build: Disable adding compatibility archive on treble builds
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1030,7 +1030,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
                              progress=progress_dict.get(block_diff.partition),
                              write_verify_script=OPTIONS.verify)
 
-  AddCompatibilityArchiveIfTrebleEnabled(input_zip, output_zip, target_info)
+  #AddCompatibilityArchiveIfTrebleEnabled(input_zip, output_zip, target_info)
 
   boot_img = common.GetBootableImage(
       "boot.img", "boot.img", OPTIONS.input_tmp, "BOOT")
@@ -1667,8 +1667,8 @@ def WriteBlockIncrementalOTAPackage(target_zip, source_zip, output_file):
   else:
     vendor_diff = None
 
-  AddCompatibilityArchiveIfTrebleEnabled(
-      target_zip, output_zip, target_info, source_info)
+  #AddCompatibilityArchiveIfTrebleEnabled(
+  #    target_zip, output_zip, target_info, source_info)
 
   # Assertions (e.g. device properties check).
   target_info.WriteDeviceAssertions(script, OPTIONS.oem_no_mount)
@@ -2180,8 +2180,8 @@ def WriteABOTAPackageWithBrilloScript(target_file, output_file,
     else:
       logger.warning("Cannot find care map file in target_file package")
 
-  AddCompatibilityArchiveIfTrebleEnabled(
-      target_zip, output_zip, target_info, source_info)
+  #AddCompatibilityArchiveIfTrebleEnabled(
+  #    target_zip, output_zip, target_info, source_info)
 
   common.ZipClose(target_zip)
 
