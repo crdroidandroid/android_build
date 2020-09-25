9d23ec63ab7fafc1e9708faa65aa80616fc4e13e
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-09-25 07:00:54 +0000

    envsetup: Prefer setting ccache compress as command line parameter
    - CCACHE_COMPRESS flag doesnt seem to show any effect, so make use of the
      direct command line argument instead.
    
    tests:
     - Before:
       cache size                          30.8 GB
    
     - After:
       cache size                          6.6 GB
    
    Change-Id: Ia180ef70400cb060680e24dabd686fb48945ec45
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1889,10 +1889,10 @@ addcompletions
 if [ -z ${CCACHE_EXEC} ]; then
     ccache_path=$(which ccache)
     if [ ! -z "$ccache_path" ]; then
-        export USE_CCACHE=1
-        export CCACHE_COMPRESS=1
+	export USE_CCACHE=1
         export CCACHE_EXEC="$ccache_path"
-        echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
+        $ccache_path -o compression=true
+	echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
     else
         echo -e "\e[31m\e[1mccache not found/installed!\e[0m"
     fi
