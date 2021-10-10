60c3c18409ef8082711076509fa571b6e8304934
jhenrique09 <jhsv09@gmail.com>
2021-10-09 23:31:54 -0300

    build: Remove su
    Change-Id: I8215272b0c462accf560efcf7ed5f1b5da3b1588
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -567,11 +567,6 @@ PRODUCT_PACKAGES_DEBUG := \
     unwind_reg_info \
     unwind_symbols \
 
-ifeq ($(LINEAGE_BUILD),)
-PRODUCT_PACKAGES_DEBUG += \
-    su
-endif
-
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \
     SettingsProvider \
