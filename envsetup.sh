5339fcf9c285c7a46ebaed0fbfed91d17f8a73af
Daniel Moral <danimoral1001@gmail.com>
2021-11-04 14:45:53 +0000

    envsetup: export the CCACHE_DIR if its not set
    Change-Id: Ie946fc6ef2b6e216831c8724d422766d0557ea3f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1891,6 +1891,9 @@ if [ -z ${CCACHE_EXEC} ]; then
     if [ ! -z "$ccache_path" ]; then
 	export USE_CCACHE=1
         export CCACHE_EXEC="$ccache_path"
+        if [ -z ${CCACHE_DIR} ]; then
+            export CCACHE_DIR=${HOME}/.ccache
+        fi
         $ccache_path -o compression=true
 	echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
     else
