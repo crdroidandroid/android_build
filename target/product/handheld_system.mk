bb85ff593d37ef2103a27474c7b2a3cb759b4f1c
ShevT <evgeny.shishkov@gmail.com>
2024-08-30 12:31:14 +0300

    Revert "Add DeviceAsWebcam to general handheld_system"
    Reason: We don't need to run additional services on devices that don't support it.
    
    This reverts commit 68923b316af008678d86baf2a96f98d41cb04e1e.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -45,7 +45,6 @@ PRODUCT_PACKAGES += \
     CaptivePortalLogin \
     CertInstaller \
     CredentialManager \
-    DeviceAsWebcam \
     DeviceDiagnostics \
     DocumentsUI \
     DownloadProviderUi \
